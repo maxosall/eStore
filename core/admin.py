af712e060bed4212085d27bbb8882effa17f3cbf
Maxo166 <sallahmad059@gmail.com>
2023-02-14 16:39:19 +0200

    extend useradmin fieldsets

--- core/admin.py
+++ core/admin.py
@@ -4,5 +4,14 @@ from .models import User
 # Register your models here.
 
 
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    pass
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "email", "first_name", "last_name"),
+            },
+        ),
+    )
