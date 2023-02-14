48f021c8f35a9fe7a2c69ad31b22860651e411c4
Maxo166 <sallahmad059@gmail.com>
2023-02-14 15:35:02 +0200

    Startapp likes

---
+++ likes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# class LikedItem(models.Model):
+
+#     def __str__(self):
+#         pass
+
+#     class Meta:
+#         db_table = ''
+#         managed = True
+#         verbose_name = 'LikedItem'
+#         verbose_name_plural = 'LikedItems'
